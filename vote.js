76562c88e8d821e73b5acfcb3d0ff57611194d24
Herpiko Dwi Aguno <herpiko@gmail.com>
2018-05-20 21:26:59 +0700

    DPT Administration.

--- vote.js
+++ vote.js
@@ -1,6 +1,7 @@
 const {createContext, CryptoFactory} = require('sawtooth-sdk/signing')
 
 const context = createContext('secp256k1')
+const request = require('request')
 const privateKey = context.newRandomPrivateKey()
 const signer = new CryptoFactory(context).newSigner(privateKey)
 
@@ -8,86 +9,93 @@ const {createHash} = require('crypto')
 const {protobuf} = require('sawtooth-sdk')
 const cbor = require('cbor')
 
-const familyName = 'vote';
+const familyName = 'ledgerLocalVote';
 
-const name = 'tpscode-' + createHash('sha256').update((new Date()).valueOf().toString()).digest('hex').substr(-8);
-console.log(name);
-const payload = {
-    Verb: 'set',
-    Name: name,
-    Value: process.argv[2]
+var vote = function(host, id, val) {
+  console.log(host);
+  console.log(id);
+  console.log(val);
+  return new Promise((resolve, reject) => {
+  
+    const payload = {
+        Verb: 'set',
+        Name: id,
+        Value: val,
+    }
+    
+    const payloadBytes = cbor.encode(payload)
+    
+    
+    /*
+        - Addresses must be a 70 character hexadecimal string
+        -The first 6 characters of the address are the first 6 characters of a sha512 hash of the IntegerKey namespace prefix: “intkey”
+        - The following 64 characters of the address are the last 64 characters of a sha512 hash of the entry Name
+    
+    */
+    
+    const payloadNameHash = createHash('sha512').update(payload.Name).digest('hex');
+    const familyNameHash = createHash('sha512').update(familyName).digest('hex');
+     
+    const transactionHeaderBytes = protobuf.TransactionHeader.encode({
+        familyName: familyName,
+        familyVersion: '1.0',
+        inputs: [familyNameHash.substr(0,6) + payloadNameHash.substr(-64)],
+        outputs: [familyNameHash.substr(0,6) + payloadNameHash.substr(-64)],
+        //inputs: ['1cf1266e282c41be5e4254d8820772c5518a2c5a8c0c7f7eda19594a7eb539453e1ed7'],
+        //outputs: ['1cf1266e282c41be5e4254d8820772c5518a2c5a8c0c7f7eda19594a7eb539453e1ed7'],
+        signerPublicKey: signer.getPublicKey().asHex(),
+        // In this example, we're signing the batch with the same private key,
+        // but the batch can be signed by another party, in which case, the
+        // public key will need to be associated with that key.
+        batcherPublicKey: signer.getPublicKey().asHex(),
+        // In this example, there are no dependencies.  This list should include
+        // an previous transaction header signatures that must be applied for
+        // this transaction to successfully commit.
+        // For example,
+        // dependencies: ['540a6803971d1880ec73a96cb97815a95d374cbad5d865925e5aa0432fcf1931539afe10310c122c5eaae15df61236079abbf4f258889359c4d175516934484a'],
+        dependencies: [],
+        payloadSha512: createHash('sha512').update(payloadBytes).digest('hex')
+    }).finish()
+    
+    const signature = signer.sign(transactionHeaderBytes)
+    
+    const transaction = protobuf.Transaction.create({
+        header: transactionHeaderBytes,
+        headerSignature: signature,
+        payload: payloadBytes
+    })
+    
+    const transactions = [transaction]
+    
+    const batchHeaderBytes = protobuf.BatchHeader.encode({
+        signerPublicKey: signer.getPublicKey().asHex(),
+        transactionIds: transactions.map((txn) => txn.headerSignature),
+    }).finish()
+    
+    
+    const signature2 = signer.sign(batchHeaderBytes)
+    
+    const batch = protobuf.Batch.create({
+        header: batchHeaderBytes,
+        headerSignature: signature2,
+        transactions: transactions
+    })
+    
+    const batchListBytes = protobuf.BatchList.encode({
+        batches: [batch]
+    }).finish()
+    
+    request.post({
+        url: 'http://' + host + ':8008/batches',
+        body: batchListBytes,
+        headers: {'Content-Type': 'application/octet-stream'}
+    }, (err, response) => {
+        if (err) return reject(err);
+        console.log(response.body)
+        resolve(response.body);
+    })
+  });
 }
 
-const payloadBytes = cbor.encode(payload)
 
-
-/*
-    - Addresses must be a 70 character hexadecimal string
-    -The first 6 characters of the address are the first 6 characters of a sha512 hash of the IntegerKey namespace prefix: “intkey”
-    - The following 64 characters of the address are the last 64 characters of a sha512 hash of the entry Name
-
-*/
-
-const payloadNameHash = createHash('sha512').update(payload.Name).digest('hex');
-const familyNameHash = createHash('sha512').update(familyName).digest('hex');
- 
-const transactionHeaderBytes = protobuf.TransactionHeader.encode({
-    familyName: familyName,
-    familyVersion: '1.0',
-    inputs: [familyNameHash.substr(0,6) + payloadNameHash.substr(-64)],
-    outputs: [familyNameHash.substr(0,6) + payloadNameHash.substr(-64)],
-    //inputs: ['1cf1266e282c41be5e4254d8820772c5518a2c5a8c0c7f7eda19594a7eb539453e1ed7'],
-    //outputs: ['1cf1266e282c41be5e4254d8820772c5518a2c5a8c0c7f7eda19594a7eb539453e1ed7'],
-    signerPublicKey: signer.getPublicKey().asHex(),
-    // In this example, we're signing the batch with the same private key,
-    // but the batch can be signed by another party, in which case, the
-    // public key will need to be associated with that key.
-    batcherPublicKey: signer.getPublicKey().asHex(),
-    // In this example, there are no dependencies.  This list should include
-    // an previous transaction header signatures that must be applied for
-    // this transaction to successfully commit.
-    // For example,
-    // dependencies: ['540a6803971d1880ec73a96cb97815a95d374cbad5d865925e5aa0432fcf1931539afe10310c122c5eaae15df61236079abbf4f258889359c4d175516934484a'],
-    dependencies: [],
-    payloadSha512: createHash('sha512').update(payloadBytes).digest('hex')
-}).finish()
-
-const signature = signer.sign(transactionHeaderBytes)
-
-const transaction = protobuf.Transaction.create({
-    header: transactionHeaderBytes,
-    headerSignature: signature,
-    payload: payloadBytes
-})
-
-const transactions = [transaction]
-
-const batchHeaderBytes = protobuf.BatchHeader.encode({
-    signerPublicKey: signer.getPublicKey().asHex(),
-    transactionIds: transactions.map((txn) => txn.headerSignature),
-}).finish()
-
-
-const signature2 = signer.sign(batchHeaderBytes)
-
-const batch = protobuf.Batch.create({
-    header: batchHeaderBytes,
-    headerSignature: signature2,
-    transactions: transactions
-})
-
-const batchListBytes = protobuf.BatchList.encode({
-    batches: [batch]
-}).finish()
-
-const request = require('request')
-
-request.post({
-    url: 'http://10.2.206.83:8008/batches',
-    body: batchListBytes,
-    headers: {'Content-Type': 'application/octet-stream'}
-}, (err, response) => {
-    if (err) return console.log(err)
-    console.log(response.body)
-})
-
+module.exports = vote;
