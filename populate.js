0dcad0824e170693eb7ce66029a5cecb21a20fdb
Herpiko Dwi Aguno <herpiko@gmail.com>
2018-05-21 01:11:52 +0700

    Default argv on populate.

--- populate.js
+++ populate.js
@@ -1,12 +1,16 @@
 const submit = require('./dpt-admin');
 const {createHash} = require('crypto');
 const faker = require('faker');
+let total = process.argv[2] || 10
+console.log('Populate to ' + total);
 
-for (var i = 1; i < 1000;i++) {
+for (var i = 0; i < total;i++) {
   let name = faker.name.findName();
   let nik = createHash('sha256').update((new Date()).valueOf().toString() + name).digest('hex');
   let uid = nik.substr(0,16) + '_' + name.replace(/\'/g, '').replace(/\./g, '').replace(/ /g, '_');
-  submit({voterId : uid, verb : 'registered', node : process.argv[2]});
+  let payload = {voterId : uid, verb : 'registered', node : process.argv[3] || 'localhost:3001'}
+  console.log(payload);
+  submit(payload);
 }
 
 
